aa68e686ac3b9ab638d667b26ffb33ce3f4f5326
ANGEL <waithiraangel78@gmail.com>
2023-06-18 13:11:09 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,6 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %d byte(s)\n", sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-printf("Size of a float: %f byte(s)\n",(unsigned long) sizeof(e));
+printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(e));
 return (0);
 }
