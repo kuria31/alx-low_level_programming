690ea95a39a8010a118e96c765653cea33a7b75e
ANGEL <waithiraangel78@gmail.com>
2023-06-18 14:10:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 #include <fcntl.h>
 /**
  * main - This is a function that prints a file using the write function.
- * Return: 1(Succes)
+ * Return: 1(Success)
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19",stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
 return (0);
 }
