46d19ea837a26ca7e027e0b2b7025b593d152af7
ANGEL <waithiraangel78@gmail.com>
2023-06-18 14:29:07 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * main - This is a function that prints a file using the write function.
  * Return: 1(Succes)
  */
-int main(void)
+void main
 {
 int a;
 a=write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 100);
