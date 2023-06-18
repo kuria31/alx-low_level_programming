99619d4028f55a27586dc503e3595a57f4f9a920
ANGEL <waithiraangel78@gmail.com>
2023-06-18 13:54:43 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
 /**
  * main - This is a function that prints a file using the write function.
  * Return: 1(Succes)
  */
 int main(void)
 {
-int a;
-a = write(1, "\"and that piece of art is useful\"\ \-\ Dora Korpar, 2015-10-19", 100);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (0);
 }
