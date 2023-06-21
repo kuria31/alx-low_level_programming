92060176f38667f3770aa0c8bb70264fb1f20b76
ANGEL <waithiraangel78@gmail.com>
2023-06-21 13:44:16 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,21 @@ void times_table(void)
 {
 	int i, j, mul, rem, num;
 
-	for (i = 0;i <= 9;i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0;j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			mul = i * j;
 			if (mul > 9)
 			{
 				rem = mul % 10;
 				num = (mul - rem) / 10;
-					_putchar(44);
-				        _putchar(32);
-					_putchar(num + '0');
-					_putchar(rem + '0');
+				_putchar(44);
+			        _putchar(32);
+				_putchar(num + '0');
+				_putchar(rem + '0');
 			}
 			else 
-			{
 				if (j != 0)
 				{
 					_putchar(44);
@@ -29,7 +28,6 @@ void times_table(void)
 					_putchar(32);
 				}
 				_putchar(mul + '0');
-			}
 		}
 		_putchar('\n');
 	}
