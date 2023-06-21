35366827b8a0c799a5401f531ec91bee9ec54d69
ANGEL <waithiraangel78@gmail.com>
2023-06-21 01:35:13 -0700

    int print_sign(int n) { if (n > 0) { _putchar(+); return (1); } else if (n == 0) { _putchar(0); return (0); } else { _putchar(-); return (-1); }

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,23 @@
 #include "main.h"
 /**
  * print_sign - a function that prints the sign of a number.
- *@n: this is an int parameter value
- *Return: 0 (Success)
+ *@n:  This is n int parameter value
+ * Return: 0 IS ZERO. 1 is greater then zero. -1 if less than zero
  */
 int print_sign(int n)
 {
-if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-else 
-_putchar('-');
-return (-1);
-}
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
