2eff2459cd39774819715c56af96ea246f18085f
ANGEL <waithiraangel78@gmail.com>
2023-06-21 12:04:27 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,8 @@ void times_table(void)
 		for (j = 0;j <= 9; j++)
 		{
 			_putchar(i * j + '0');
-			_putchar(i * j + '0');
-			j++;
 		}
-		i++;
+		_putchar(i * j + '0');
+		_putchar('\n');
 	}
 }
