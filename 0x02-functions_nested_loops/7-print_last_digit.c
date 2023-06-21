0cd69e77dcf518d7c9cac315af280a8e2c02e4b3
ANGEL <waithiraangel78@gmail.com>
2023-06-21 04:18:00 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,14 @@ int print_last_digit(int n)
 	int last_digit;
 
 	last_digit = n % 10;
+	if (last_digit > 0)
+	{
 	_putchar(last_digit + 48);
 	return (last_digit);
+	}
+	else 
+	{
+		_putchar(-last_digit + 48);
+		return (-last_digit);
+	}
 }
