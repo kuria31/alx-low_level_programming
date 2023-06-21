e53d5c78b61325ffa00efbd805b5dec14711fa7c
ANGEL <waithiraangel78@gmail.com>
2023-06-21 13:59:30 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - addtion of two numbers
+ * @a: a number
+ * @b: another number
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int sum;
+	sum = a + b;
+	return (sum);
+}
