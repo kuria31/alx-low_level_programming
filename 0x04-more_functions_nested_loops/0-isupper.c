8857acff215019d62f8bc9b643d310b523f456a4
ANGEL <waithiraangel78@gmail.com>
2023-06-22 04:47:05 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,16 @@
 /**
  * _isupper - function that checks for uppercase character
  * @c: letter to check
- * Return: 0
+ * Return: 0 if lowercase and 1 if lowercase
  */
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
