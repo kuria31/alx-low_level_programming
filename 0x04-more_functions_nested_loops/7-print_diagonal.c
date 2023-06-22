c7e635279c16b558c6c70ab319e56cd3fbbf9930
ANGEL <waithiraangel78@gmail.com>
2023-06-22 10:57:22 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,13 +6,17 @@
  */
 void print_diagonal(int n)
 {
-	int let;
+	int let, space;
 
 	if (n > 0)
 	{
 		for (let = 0; let <= n; let++)
 		{
+			for (space = 0; space < let; space++)
+			{
+			_putchar(32);
 			_putchar(92);
+			}
 		}
 		_putchar('\n');
 	}
