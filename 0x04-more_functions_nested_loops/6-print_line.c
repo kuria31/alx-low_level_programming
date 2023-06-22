03114c5e0a0d12e6922e4fa3a4ec24b304a74e3f
ANGEL <waithiraangel78@gmail.com>
2023-06-22 06:48:42 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - function that draws a straight line in the terminal
+ * @n: the variamble used to print the line
+ * Return: 0
+ */
+void print_line(int n)
+{
+	for (n = 0; n >= 10; n++)
+	{
+		if (n == 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
