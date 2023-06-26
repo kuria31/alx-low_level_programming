417b113f1a23ba0c9ded4565d93f5bd9fe6155c7
ANGEL <waithiraangel78@gmail.com>
2023-06-26 13:18:37 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts -  function that prints a string, followed by a new line
+ * @str: A string
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	_putcahr(str);
+	_putchar('\n');
+}
