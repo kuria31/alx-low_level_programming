9ba59b98ddffa99fc61e3bfd5bec50605f891ce2
ANGEL <waithiraangel78@gmail.com>
2023-06-29 11:58:09 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,20 +7,18 @@
 */
 char *_strcat(char *dest, char *src)
 {
-char str[1000];
-int i = 0, j = 0;
-while (dest[i] != '\0')
-{
-str[j] = dest[i];
-i++;
-j++;
-}
-i = 0;
-while (src[i] != '\0')
-{
-str[j] = src[i];
-i++;
-j++;
-}
-str[j] != '\0';
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+		while (src[j] != '\0')
+		{
+			src[j] = dest[i];
+			i++;
+			j++;
+		}
+	}
+	src[j] = '\0';
+	return (dest);
 }
