e34261efb8894efd046e38d771859a7323e19461
ANGEL <waithiraangel78@gmail.com>
2023-06-29 13:51:48 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,19 +9,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	while (s1[i] == s2[i] && s1[i] == '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1 - s2);
+		}
 		i++;
-
-	if (s1[i] < s2[i])
-	{
-		return (s1 < s2);
 	}
-	else if (s1[i] > s2[i])
-	{
-		return (s2 > s1);
-	}
-	else
-	{
-		return (s1 == s2);
-	}
+	return (0);
 }
