cd193c3d7c0bceae1f5638503bf085bf75da8164
ANGEL <waithiraangel78@gmail.com>
2023-07-18 12:26:31 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - function that creates a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: 0
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+
+	new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	new_dog->name = strdup(name);
+	new_dog->age = age;
+	new_dog->owner = strdup(owner);
+
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+	return (new_dog);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog -  a function that frees dogs.
+ * @d: a pointer to struct dog
+ * Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
