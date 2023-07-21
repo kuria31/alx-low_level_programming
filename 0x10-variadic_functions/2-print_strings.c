0742a07cf1f3b4dec379c44572f13dd5f842c7a7
ANGEL <waithiraangel78@gmail.com>
2023-07-21 11:33:43 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", string);
 		}
-		if (i < n - 1 && separator != NULL)
+		if (i < n - 1 && separator != NULL && va_arg(args, char *) != NULL)
 		{
 			printf("%s", separator);
 		}
