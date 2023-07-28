df5677ab250acfd7f65f7e430ba61d8a70a6987e
ANGEL <waithiraangel78@gmail.com>
2023-07-28 11:18:47 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "list.h"
+/**
+ *  print_list - function that prints all the elements
+ *  @h: the head
+ *  Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		i++;
+		}
+	}
+		return (i);
+}
