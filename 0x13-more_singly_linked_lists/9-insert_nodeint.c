1dd9e553806755bab386ed9ab41fed666fb9cf94
ANGEL <waithiraangel78@gmail.com>
2023-07-31 11:56:01 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 		count++;
 	}
-	while (current == NULL || current->next)
+	while (current == NULL || current->next == NULL)
 	{
 		free(newNode);
 		return (NULL);
